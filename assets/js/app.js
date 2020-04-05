960fac33e0e483619fcccdd0a8eb36f83b805840
husubayli-elvin <husubayli@gmail.com>
2020-04-05 23:02:53 +0400

    navbar tam hazirdi

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,2 +1,16 @@
-let body = document.querySelector('body').style.minWidth = "1036px"
-console.log(body);
+    document.querySelector("#burger-btn").addEventListener('click', function(){
+        document.querySelector('.nav-right').classList.add('visible');
+    });
+
+    document.querySelector('.burger-nav-btn').addEventListener('click', function(){
+        document.querySelector('.nav-right').classList.remove('visible');
+    });
+
+    $(window).scroll(function (event) {
+        if ($(window).scrollTop() > 300) {
+            $('#main-menu-search-bar-wrapper').addClass('main-menu-fixed');
+            $('.nav-content-hr').css('display', 'none')
+        } else {
+            $('#main-menu-search-bar-wrapper').removeClass('main-menu-fixed');
+        }
+    });
